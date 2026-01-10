1698b39f734c9845b13947de3af2f8554f05dc52
Michael Keyes <mgkeyes@vigovproductions.net>
2026-01-10 19:24:04 +0000

    Add `MODETEST.COM` utility for testing / debugging
    As per https://github.com/PluMGMK/vbesvga.drv/issues/46#issuecomment-3499049225

--- release-work/make-release.sh
+++ release-work/make-release.sh
@@ -94,12 +94,16 @@ mv -v WIN16DDK/386/VBEGRAB/VBEVMDIB.SYM relsyms
 # Build the tools as well
 VIDMODES_COMMIT=`git log -1 --pretty=format:"%h" ../VIDMODES.ASM`
 git status --short ../VIDMODES.ASM | grep "^ M" && VIDMODES_COMMIT="$VIDMODES_COMMIT(modified)"
+MODETEST_COMMIT=`git log -1 --pretty=format:"%h" ../MODETEST.ASM`
+git status --short ../MODETEST.ASM | grep "^ M" && MODETEST_COMMIT="$MODETEST_COMMIT(modified)"
 jwasm -bin ../AUXSTACK.ASM
 mv -v AUXSTACK.BIN AUXSTACK.COM
 jwasm -bin ../AUXCHECK.ASM
 mv -v AUXCHECK.BIN AUXCHECK.COM
 jwasm -DVIDMODES_COMMIT=\'$VIDMODES_COMMIT\' -bin ../VIDMODES.ASM
 mv -v VIDMODES.BIN VIDMODES.COM
+jwasm -DMODETEST_COMMIT=\'$MODETEST_COMMIT\' -bin ../MODETEST.ASM
+mv -v MODETEST.BIN MODETEST.COM
 jwasm -bin ../SETUP.ASM
 mv -v SETUP.BIN SETUP.EXE # !
 
